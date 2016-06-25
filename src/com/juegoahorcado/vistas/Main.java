8fee5fe78b31ac8f9a2614cccb47820bb8670555
alejandro cano <alejandro cano>
2016-06-25 16:33:40 -0300

    Agregado interfaz por sistema operativo

--- src/com/juegoahorcado/vistas/Main.java
+++ src/com/juegoahorcado/vistas/Main.java
@@ -3,12 +3,22 @@
  */
 package com.juegoahorcado.vistas;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+        try {
+			UIManager.setLookAndFeel(
+			        UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
 		new MenuPrincipal();
 	}
 
--- src/com/juegoahorcado/vistas/MenuJuego.java
+++ src/com/juegoahorcado/vistas/MenuJuego.java
@@ -4,8 +4,6 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Hashtable;
